709bb7ab3e39d36a1a89613e6feb92ca6336f005
Anton <onepix2000@gmail.com>
2019-10-10 09:40:45 +0200

    Initial Commit

---
+++ Protocols example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Protocols example
+//
+//  Created by Test on 10/10/2019.
+//  Copyright © 2019 Peanch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
